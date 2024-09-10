de570355088336995bb50df7a4377de1a39c370f
JagersEgo <148036210+JagersEgo@users.noreply.github.com>
2024-09-10 20:47:40 +1000

    test stage changes + music man change

--- Scenes/music manager example/stage_1.gd
+++ Scenes/music manager example/stage_1.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var music_manager = $MusicManager  # Get the MusicManager node
-	music_manager.play_stage_music("stage1")  # Play music for stage1
+	music_manager.play_stage_music("stage2")  # Play music for stage1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- systems/Music/music_manager.gd
+++ systems/Music/music_manager.gd
@@ -3,7 +3,7 @@ extends Node
 # A dictionary to map stage names to their respective audio files.
 var stage_music = {
 	"stage1": "res://content/music/Schlatts_Basement.mp3",
-	#"stage2": "res://music/stage2.ogg",
+	"stage2": "res://content/music/Geneburn (Hard asf (copyrighted))/The_Blood_We_Spill.mp3",
 	#"stage3": "res://music/stage3.ogg"
 }
 
@@ -18,7 +18,7 @@ func play_stage_music(stage_name: String) -> void:
 		if audio_stream:
 			audio_player.stream = audio_stream
 			audio_player.play()  # Play the music
-			print("Now playing: " + stage_name)
+			print("Now playing: " + stage_music[stage_name])
 		else:
 			print("Failed to load music file for: " + stage_name)
 	else:
@@ -27,3 +27,32 @@ func play_stage_music(stage_name: String) -> void:
 # Function to stop the music
 func stop_music() -> void:
 	audio_player.stop()
+
+# Function to pause the music
+func pause_music() -> void:
+	if audio_player.playing:
+		audio_player.pause()
+		print("Music paused")
+	else:
+		print("Music is not currently playing")
+
+# Function to resume the paused music
+func resume_music() -> void:
+	if !audio_player.playing and audio_player.stream:
+		audio_player.play()
+		print("Music resumed")
+
+func set_volume(volume_db: float) -> void:
+	#Sets the volume of the current music playback.
+#
+	#Parameters:
+	#volume_db (float): Volume in decibels (dB).
+					   #- 0 dB is the default volume.
+					   #- Negative values reduce the volume.
+					   #- Values below -80 dB mute the sound
+	#Examples:
+	#set_volume(0)    # Full volume
+	#set_volume(-10)  # Reduced volume
+	#set_volume(-80)  # Mute
+	audio_player.volume_db = volume_db
+	print("Volume set to: " + str(volume_db) + " dB")
