94d7d7ee852129f0d4344152fc30328c831877fa
taigaclaw <coolq1000@gmail.com>
2024-09-26 21:35:22 +1000

    added player auto-step

--- player/player_movement_controller.gd
+++ player/player_movement_controller.gd
@@ -1,7 +1,10 @@
-extends Node
+extends Node2D
 class_name PlayerMovementController
 
 @export var character: CharacterBody2D
+
+# Step up
+@export var auto_step_size: float = 3
 
 # Horizontal motion
 @export var move_speed: float = 200
@@ -26,8 +29,14 @@ var jumped: bool = false
 var auto_jump_time: float = 0
 var jump_grace_time: float = 0
 
+# Connections
+@onready var bottom_left_ray: RayCast2D = $"../CollisionShape2D/BottomLeft"
+@onready var bottom_right_ray: RayCast2D = $"../CollisionShape2D/BottomRight"
+@onready var top_left_ray: RayCast2D = $"../CollisionShape2D/TopLeft"
+@onready var top_right_ray: RayCast2D = $"../CollisionShape2D/TopRight"
+
 # Input variables
-var horizontal_axis
+var horizontal_axis: float
 
 var vertical_dictionary: Dictionary = {
 	"move_up_pressed": false,
@@ -42,10 +51,11 @@ func _ready() -> void:
 func _set_input(h_axis: float, v_dictionary: Dictionary) -> void:
 	horizontal_axis = h_axis
 	vertical_dictionary = v_dictionary
-	
+
 func _physics_process(delta: float) -> void:
 	calculate_horizontal_movement(delta)
 	calculate_vertical_movement(delta)
+	step_check(character.velocity.x, delta)
 	character.move_and_slide()
 	PlayerStatsManager._set_player_position(character.position)
 
@@ -112,6 +122,51 @@ func calculate_vertical_movement(delta: float):
 	if jumped and vertical_dictionary["move_up_released"] and character.velocity.y < 0:
 		character.velocity *= min_jump_height / max_jump_height
 		jumped = false
+
+func step_check(horizontal_stride: float, delta: float):
+	# Figure out which raycast to use
+	var bottom_ray: RayCast2D = null
+	var top_ray: RayCast2D = null
+	var step_possible: bool = true
+	
+	if horizontal_stride < 0:
+		bottom_ray = bottom_left_ray
+		top_ray = top_left_ray
+	elif horizontal_stride > 0:
+		bottom_ray = bottom_right_ray
+		top_ray = top_right_ray
+	else:
+		step_possible = false
+	
+	# Check step can even happen
+	if not step_possible:
+		return
+	
+	# Make sure the raycasts are frame accurate
+	bottom_ray.force_raycast_update()
+	top_ray.force_raycast_update()
+	
+	# Make sure it's possible for us to step into something
+	if not bottom_ray.is_colliding():
+		return
+	
+	var bottom_point: Vector2 = bottom_ray.get_collision_point()
+	var bottom_length: float = bottom_ray.global_position.distance_to(bottom_point)
+	
+	# Will we step into something?
+	if bottom_length > abs(horizontal_stride * delta):
+		return
+	
+	# Check if something is blocking it
+	if top_ray.is_colliding():
+		# Figure out how steep the step is
+		var top_point: Vector2 = top_ray.get_collision_point()
+		var step_size = bottom_ray.global_position.y - top_point.y
+		
+		if step_size <= auto_step_size:
+			# Move player up and make sure they don't continue falling
+			character.position.y -= step_size + 2
+			character.velocity.y = min(character.velocity.y, 0)
 
 func jump():
 	character.velocity.y = -max_jump_height
