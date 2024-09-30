32173f4501f7899bed7c7d452889ba853e29ed02
ChalaHeal <jeffb3d@gmail.com>
2024-09-30 21:12:35 +1000

    Merge

--- player/input_manager.gd
+++ player/input_manager.gd
@@ -6,8 +6,6 @@ extends Node
 # Movement variables
 var horizontal_axis
 
-var dashing = false
-
 var vertical_dictionary: Dictionary = {
 	"move_up_pressed": false,
 	"move_up_released": false,
@@ -36,41 +34,34 @@ func _physics_process(delta: float) -> void:
 func _get_input(delta: float) -> void:
 	horizontal_axis = Input.get_axis("move_left", "move_right")
 	
-	if Input.is_action_just_pressed("dash") and dashing == false:
-		dashing = true
-	else: # collect inputs if not dashing
-		if Input.is_action_just_pressed("move_up"):
-			vertical_dictionary["move_up_pressed"] = true
-			vertical_dictionary["move_up_held"] = true;
-		else:
-			vertical_dictionary["move_up_pressed"] = false
-				
-		if Input.is_action_just_released("move_up"):
-			vertical_dictionary["move_up_released"] = true
-			vertical_dictionary["move_up_held"] = false;
-		else:
-			vertical_dictionary["move_up_released"] = false
-		
-		if Input.is_action_just_pressed("move_down"):
-			vertical_dictionary["move_down_pressed"] = true
-		else:
-			vertical_dictionary["move_down_pressed"] = false
-		
-		if Input.is_action_just_released("move_down"):
-			vertical_dictionary["move_down_released"] = true
-		else:
-			vertical_dictionary["move_down_released"] = false
-
+	if Input.is_action_just_pressed("move_up"):
+		vertical_dictionary["move_up_pressed"] = true
+		vertical_dictionary["move_up_held"] = true;
+	else:
+		vertical_dictionary["move_up_pressed"] = false
+			
+	if Input.is_action_just_released("move_up"):
+		vertical_dictionary["move_up_released"] = true
+		vertical_dictionary["move_up_held"] = false;
+	else:
+		vertical_dictionary["move_up_released"] = false
 	
+	if Input.is_action_just_pressed("move_down"):
+		vertical_dictionary["move_down_pressed"] = true
+	else:
+		vertical_dictionary["move_down_pressed"] = false
+	
+	if Input.is_action_just_released("move_down"):
+		vertical_dictionary["move_down_released"] = true
+	else:
+		vertical_dictionary["move_down_released"] = false
+
 
 func _process_movement(delta: float) -> void:
 	movement_controller._set_input(horizontal_axis, vertical_dictionary)
 	
 func get_inventory():
 	return inventory
-
-func player_bounced() -> void:
-	movement_controller.jump()
 
 func _on_health_component_take_damage() -> void:
 	print("Player was hit")
